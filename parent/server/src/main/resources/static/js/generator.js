8fbb9c1f238b24464469bd8cffa071b5c25019f6
ForeverLTP <hews98@126.com>
2018-10-24 22:37:23 +0800

    1

--- parent/server/src/main/resources/static/js/generator.js
+++ parent/server/src/main/resources/static/js/generator.js
@@ -1,14 +1,15 @@
 $(function(){
 	$(".addTable").on("click",".addButton",function(){
-		 
+		var data ="<div class=''>"
+		var table = "<table class='table table-bordered ' style='overflow: auto;'>";
+		 title="<thead><tr><td>"+$(".clumnName").val()+"</td><td>"+$(".type").val()+"</td></tr></thead>";
 		 layer.open({
 			  type: 1 //Page层类型
-			  //,area: ['500px', '300px']
 			  ,title: $(".createTable").val()
 			  ,shade: 0.6 //遮罩透明度
 			  ,maxmin: true //允许全屏最小化
 			  ,anim: 1 //0-6的动画形式，-1不开启
-			  ,content: $(".table")
+			  ,content: ""
 			});
 	});
 })

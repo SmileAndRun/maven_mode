2f9d78b6a77d48148a56c678a4dc988abd682181
ForeverLTP <hews98@126.com>
2018-10-25 17:51:17 +0800

    code init

--- parent/server/src/main/java/com/bdcom/server/common/InitLucene.java
+++ parent/server/src/main/java/com/bdcom/server/common/InitLucene.java
@@ -1,29 +1,17 @@
 package com.bdcom.server.common;
 
 import java.io.IOException;
-import java.util.List;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.bdcom.server.lucene.IndexUtils;
-import com.bdcom.server.model.MysqlModel;
-import com.bdcom.server.utils.JDBCUtils;
+
 
 public class InitLucene {
 
 	public static void main(String[]args){
-		/*try {
+		try {
 			new IndexUtils().creatIndex();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}*/
-		/*String url = "jdbc:mysql://localhost:3306/xlt?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8";
-		String sql = "CREATE TABL `test` (`test`  int NULL ,PRIMARY KEY (`test`));";
-		//String sql = "update t_barrage set content = '628' where contentId = 2;";
-		//String sql = "select * from test;";
-		//String sql = "drop table test;";
-		JDBCUtils.excuteSql(url, sql);*/
-			
-		
+		}
 	}
 }
--- parent/server/src/main/resources/static/js/generator.js
+++ parent/server/src/main/resources/static/js/generator.js
@@ -1,15 +1,63 @@
 $(function(){
 	$(".addTable").on("click",".addButton",function(){
-		var data ="<div class=''>"
-		var table = "<table class='table table-bordered ' style='overflow: auto;'>";
-		 title="<thead><tr><td>"+$(".clumnName").val()+"</td><td>"+$(".type").val()+"</td></tr></thead>";
+		var data ="<div class='data'>";
+		var button = "<div><button class='add'><i class='fa fa-plus-square fa-2x' aria-hidden='true'></i>"+$(".add").val()+"</button><button class='del'><i class='fa fa-minus-square fa-2x' aria-hidden='true'></i>"+$(".delete").val()+"</button></div>";
+		var table = "<table class='table table-bordered tableData' style='overflow: auto;'><thead><tr><td>"+$(".clumnName").val()+"</td><td>"+$(".type").val()+"</td></tr></thead></table>";
+		data += button + table + "</div>";
 		 layer.open({
 			  type: 1 //Page层类型
+			  , area: ['420px', '240px']//宽高
 			  ,title: $(".createTable").val()
 			  ,shade: 0.6 //遮罩透明度
 			  ,maxmin: true //允许全屏最小化
 			  ,anim: 1 //0-6的动画形式，-1不开启
-			  ,content: ""
+			  ,content: data
+			  ,btn: ['确定', '取消'] //按钮组
+		 	  ,yes: function(index, layero){
+		 		  if($(".tableData").find("input")[0] == null || $(".tableData").find("input")[0] == undefined){
+		 			  layer.msg("table is null,please add clumn!")
+		 		  }else{
+		 			  if($(".rowSelected").val() == ""){
+		 				 layer.msg("please add completed!")
+		 			  }else{
+		 				 layer.close();
+		 			  }
+		 		  }
+			  }
 			});
+	});
+	$(this).on("click",".add",function(){
+		var input = $(".tableData").find("tr:last").find("td input");
+		if(input != undefined  && input != null){//当前字段填写后才能添加下一个
+			if(input.val() == ""){
+				return ;
+			}
+		}
+		var select = "<select>" +
+				"<option value='tinyint'>tinyint</option><option value='smallint'>smallint</option>" +
+				"<option value='mediumint'>mediumint</option><option value='int'>int</option>" +
+				"<option value='integer'>integer</option><option value='bigint'>bigint</option>" +
+				"<option value='float'>float</option><option value='double'>double</option>" +
+				"<option value='decimal'>decimal</option><option value='date'>date</option>" +
+				"<option value='time'>time</option><option value='year'>year</option>" +
+				"<option value='datetime'>datetime</option><option value='timestamp'>timestamp</option>" +
+				"<option value='char'>char</option><option value='varchar'>varchar</option>" +
+				"<option value='tinyblob'>tinyblob</option><option value='tinytext'>tinytext</option>" +
+				"<option value='blob'>blob</option><option value='text'>text</option>" +
+				"<option value='mediumblob'>mediumblob</option><option value='mediumtext'>mediumtext</option>" +
+				"<option value='longblob'>longblob</option><option value='longtext'>longtext</option>" 
+		+"</select>";
+		var row = "<tr><td><input type=\"text\"/></td><td>"+select+"</td></tr>";
+		$(".tableData").append(row);
+	});
+	$(this).on("click",".tableData input",function(){
+		$(".rowSelected").removeClass();
+		$(this).addClass("rowSelected");
+	});
+	
+	$(this).on("click",".del",function(){
+		
+	 $(".rowSelected").parent("td").parent("tr").remove();
+		
 	});
 })

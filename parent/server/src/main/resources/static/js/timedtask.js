926fff9eee429794f5a19dd5a5db28d39372e093
ForeverLTP <hews98@126.com>
2018-11-23 17:53:31 +0800

    时间控件

--- parent/server/src/main/resources/static/js/timedtask.js
+++ parent/server/src/main/resources/static/js/timedtask.js
@@ -1,17 +1,27 @@
 $(function(){
 	$(".titleButton").on("click",".createButton",function(){
-		var data = "";
-		data = "<table><tr><td><lable for='taskNameVal'>"+$(".taskName").val()+"</lable></td><td><input type='text' class='taskNameVal'/></td></tr>" +
-				"<tr><td><lable for='jobTypeVal'>"+$(".jobType").val()+"</lable></td><td><input type='text' class='jobTypeVal'/></td></tr>" +
-				"<tr><td><lable for='createTimeVal'>"+$(".createTime").val()+"</lable></td><td><input type='text' class='createTimeVal'/></td></tr>" +
-				"<tr><td><lable for='finishedTimeVal'>"+$(".finishedTime").val()+"</lable></td><td><input type='text' class='finishedTimeVal'/></td></tr>" +
-				"<tr><td><lable for='executionFrequencyVal'>"+$(".executionFrequency").val()+"</lable></td><td><input type='text' class='executionFrequencyVal'/></td></tr>" +
-				"</table>"
-		layer.open({
-			  title: $(".add").val()
-			  ,content: data
-			  ,area: ['500px', '300px']
-			});   
-		$(".layui-layer").css("border-radius", "8px");
+		$(".shadeDiv").css("display","block");
+		$(".addDiv").css("display","block");
 	});
+	$(".addJobTable").on("click",".cancel",function(){
+		$(".shadeDiv").css("display","none");
+		$(".addDiv").css("display","none");
+		$(".taskNameVal").val("");
+		$(".finishedTimeVal").val("");
+		$(".createTimeVal").val("");
+		$(".executionFrequencyVal").val("");
+	})
+	initDateTimePicker(".startTimeDiv");
+	initDateTimePicker(".endTimeDiv");
+	function initDateTimePicker(id){
+		$(id).datetimepicker({
+	        format: "yyyy-mm-dd  HH:mm:ss",
+	        showMeridian: true,
+	        autoclose: true,
+	        todayBtn: true,
+	        pickerPosition: "bottom-left"
+	    });
+	}
+	
+	
 })

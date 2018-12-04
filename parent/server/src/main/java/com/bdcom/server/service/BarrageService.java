8bb7766ef1ffd2576ad48b0884dfa2da13174ad5
ForeverLTP <hews98@126.com>
2018-12-04 17:57:26 +0800

    完善

---
+++ parent/server/src/main/java/com/bdcom/server/service/BarrageService.java
@@ -0,0 +1,41 @@
+package com.bdcom.server.service;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.common.model.Barrage;
+
+
+
+
+public interface BarrageService {
+	
+	/**
+	 * 获取所有弹幕
+	 * @return
+	 */
+	public List<Barrage> getAllBar();
+	/**
+	 * 通过时间模糊获取部分弹幕
+	 * @param time
+	 * @return
+	 */
+	public List<Barrage> getListBarByTime(Date time);
+	/**
+	 * 通过id获取弹幕
+	 */
+	public Barrage getBarById(String id);
+	/**
+	 * 模糊查询（待定）
+	 */
+	public List<Barrage> getListBarByLike(String content);
+	/**
+	 * 查询当前时间的弹幕数量
+	 * @param time
+	 * @return
+	 */
+	public int getBarrageCount(Timestamp time);
+
+	
+}

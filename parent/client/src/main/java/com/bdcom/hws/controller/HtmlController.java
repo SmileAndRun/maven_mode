5c21e18f8ca015d883da272bc743a2e45d0a7325
ForeverLTP <hews98@126.com>
2018-12-28 17:15:35 +0800

    解决github高位漏洞及初始化聊天室

--- parent/client/src/main/java/com/bdcom/hws/controller/HtmlController.java
+++ parent/client/src/main/java/com/bdcom/hws/controller/HtmlController.java
@@ -9,6 +9,7 @@ import org.common.model.Barrage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bdcom.server.service.BarrageService;
 
@@ -16,37 +17,15 @@ import com.bdcom.server.service.BarrageService;
 @Controller
 public class HtmlController {
 	
-	@Autowired
-	private BarrageService barService;
-	
 	@RequestMapping(value="/")
-	public String initLogin(){
-		
-		return "login";
+	public ModelAndView initLogin(){
+		ModelAndView model = new ModelAndView("login");
+		return model;
 	}
-	
-	@RequestMapping(value="/index")
-	public String initHomePage(HttpServletRequest req){
-		/*            适合不跳转页面
-		 * JSONObject jsonObj = new JSONObject();
-		List<Barrage> barList = barService.getAllBar();
-		jsonObj.put("result", barList);
-		response.setCharacterEncoding("UTF-8");
-		JsonUtils.writeJson(response, jsonObj);*/
-		
-		List<Barrage> barList = barService.getAllBar();
-		req.setAttribute("barList", barList);
-		
-		return "index";
-	}
-	@RequestMapping(value="/home")
-	public String initIndexPage(HttpServletRequest req){
-		return "index1";
-	}
-	@RequestMapping(value="/test")
-	public String test(){
-		
-		return "index";
+	@RequestMapping(value="/html/chatroom")
+	public ModelAndView initHomePage(HttpServletRequest req){
+		ModelAndView model = new ModelAndView("chatroom");
+		return model;
 	}
 
 }

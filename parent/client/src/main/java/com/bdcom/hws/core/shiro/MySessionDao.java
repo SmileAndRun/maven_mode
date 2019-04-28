dba4ed8dd016934948ee9de2dfe5c5f7d243e3bb
ForeverLTP <hews98@126.com>
2019-04-28 13:57:48 +0800

    修复sso session bug

--- parent/client/src/main/java/com/bdcom/hws/core/shiro/MySessionDao.java
+++ parent/client/src/main/java/com/bdcom/hws/core/shiro/MySessionDao.java
@@ -1,14 +1,24 @@
 package com.bdcom.hws.core.shiro;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import org.apache.shiro.session.Session;
 import org.apache.shiro.session.mgt.eis.MemorySessionDAO;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
+import org.common.model.Log;
+import org.common.model.OnlineCountModel;
+import org.common.model.client.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bdcom.hws.service.LogService;
+import com.bdcom.hws.service.UserService;
 
 
 public class MySessionDao extends MemorySessionDAO {
@@ -22,12 +32,20 @@ public class MySessionDao extends MemorySessionDAO {
         this.sessions = new ConcurrentHashMap<Serializable, Session>();
     }
 
+    @Autowired
+    LogService ls;
+    @Autowired
+    UserService us;
+    
     //重写方法
     protected Serializable doCreate(Session session) {
     	log.info("shiro开始创建session");
         Serializable sessionId = generateSessionId(session);
         assignSessionId(session, sessionId);
         storeSession(sessionId, session);
+        synchronized(session){
+       	 OnlineCountModel.activeSessions++;
+		}
     	log.info("shiro session创建完成");
         return sessionId;
     }
@@ -41,6 +59,22 @@ public class MySessionDao extends MemorySessionDAO {
             sessions.remove(id);
             log.info("shiro删除session完成");
         }
+        String userName = String.valueOf(session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY));
+        beforeCache.remove(userName);
+        synchronized(session){
+        	 OnlineCountModel.activeSessions--;
+		}
+        Date date = new Date();
+		Log log = new Log();
+		if(userName != null){
+			User user = us.getUserByUname(userName);
+			log.setUserid(user.getUserId());
+			log.setLogtype("2");
+			log.setLogmessage("loginout");
+			log.setLogtime(new Timestamp(date.getTime()));
+			log.setLogiserror("0");
+			ls.insertLog(log);
+		}
     }
 	public Map<String, Session> getBeforeCache() {
 		return beforeCache;

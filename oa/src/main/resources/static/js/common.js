9ddb4a09c382a526270da2e1dacd72f77714171f
ForeverLTP <hews98@126.com>
2019-06-23 17:43:23 +0800

    添加国际化事件支持及调整中英文文件位置

--- oa/src/main/resources/static/js/common.js
+++ oa/src/main/resources/static/js/common.js
@@ -1,6 +1,23 @@
+var menu = {};
 $(function(){
 	$(".menu").on("click","ul li",function(){
 		$(".menu ul li").find(".active").removeClass("active");
 		$(this).addClass("active");
-	})
+	});
+	
 })
+//中英文国际化
+menu.changeLanguage = function (lan){
+	var url = "/i18n/selectLanguage";
+	var data = {
+			language: lan
+	}
+	var s_function = function(){
+		location.reload();
+	}
+	var e_function = function(){
+		
+	}
+	jQuery.commonAjax(url,data,s_function,e_function);
+	
+}
